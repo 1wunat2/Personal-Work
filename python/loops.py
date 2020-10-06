975a566ed02fa9f50c5159243e5af90895363348
1wunat2 <47333260+1wunat2@users.noreply.github.com>
2020-10-05 21:10:34 -0400

    added code

---
+++ python/loops.py
@@ -0,0 +1,17 @@
+# for x in range (4,10):
+#     print(x)
+
+# string = "Hello World"
+# for x in string:
+#     print(x)
+
+# primes = [2,3,5,7]
+# for primes in primes:
+#     print(primes)
+
+# x = 4
+# while x <= 10:
+#     print(x)
+#     if x == 7:
+#         break
+#     x += 1
